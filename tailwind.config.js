9bdb874d57a24ae47464fb554d8650d6fa60a899
michaelheckmann <heckmannmichael@yahoo.de>
2024-11-25 12:30:34 +0100

    init comit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts}"],
+  theme: {
+    fontFamily: {
+      sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
+      serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+    },
+    extend: {},
+  },
+  plugins: [addVariablesForColors],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+  addBase({ ":root": newVars });
+}
