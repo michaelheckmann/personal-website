061a37d93c85803cf6c1d536629babbd01783aa8
michaelheckmann <heckmannmichael@yahoo.de>
2026-03-31 13:17:53 +0200

    add loops

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 import { validate } from "disposable-email";
+// import { LoopsClient, APIError } from "loops";
 
 export const server = {
   subscribeNewsletter: defineAction({
@@ -17,29 +18,29 @@ export const server = {
       // await sleep(5000);
       // return true;
 
-      const url = `https://api.beehiiv.com/v2/publications/${env.BEEHIIV_PUB_ID}/subscriptions`;
-      const res = await fetch(url, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${env.BEEHIIV_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          referring_site: "https://heckmann.app",
-        }),
-      });
+    //   const url = `https://api.beehiiv.com/v2/publications/${env.LOOPS_API_KEY}/subscriptions`;
+    //   const res = await fetch(url, {
+    //     method: "POST",
+    //     headers: {
+    //       Authorization: `Bearer ${env.BEEHIIV_API_KEY}`,
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       email,
+    //       referring_site: "https://heckmann.app",
+    //     }),
+    //   });
 
-      if (!res.ok) {
-        throw new Error("Failed to subscribe to newsletter");
-      }
+    //   if (!res.ok) {
+    //     throw new Error("Failed to subscribe to newsletter");
+    //   }
 
-      const { data } = await res.json();
-      if (!data?.id) {
-        throw new Error("Failed to subscribe to newsletter");
-      }
+    //   const { data } = await res.json();
+    //   if (!data?.id) {
+    //     throw new Error("Failed to subscribe to newsletter");
+    //   }
 
-      return true;
-    },
+    //   return true;
+    // },
   }),
 };
